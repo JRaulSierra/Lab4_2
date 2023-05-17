60dac30c271db48cab7cfb7b2bb58cff3f6e11f0
Raul Sierra <70240223+JRaulSierra@users.noreply.github.com>
2023-05-17 16:23:53 -0600

    Add files via upload

---
+++ Lab4_2/Lab4_2/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Lab4_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(1, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(799, 448);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AccessibleName = "tabPageMenu";
+            this.tabPage1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPage1.BackgroundImage")));
+            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(791, 419);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Menu";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightSalmon;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(477, 248);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(203, 139);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Historial";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.btnHistorial_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSalmon;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(110, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(203, 139);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ajustes";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.btnAjustes_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSalmon;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(477, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 139);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Bebida";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btnBebida_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSalmon;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(110, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 139);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Comida";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnComida_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.AccessibleName = "tabPageComida";
+            this.tabPage2.BackgroundImage = global::Lab4_2.Properties.Resources.pagina_web_para_restaurante;
+            this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(791, 419);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Comida";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackgroundImage = global::Lab4_2.Properties.Resources.pagina_web_para_restaurante;
+            this.tabPage3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage3.Controls.Add(this.button7);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(791, 419);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Bebida";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackgroundImage = global::Lab4_2.Properties.Resources.pagina_web_para_restaurante;
+            this.tabPage4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage4.Controls.Add(this.button8);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(791, 419);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Ajustes";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackgroundImage = global::Lab4_2.Properties.Resources.pagina_web_para_restaurante;
+            this.tabPage5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage5.Controls.Add(this.button9);
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(791, 419);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Historial";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Khaki;
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.Location = new System.Drawing.Point(732, 6);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(51, 48);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.btnOpciones_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Khaki;
+            this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.Location = new System.Drawing.Point(732, 6);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(51, 48);
+            this.button7.TabIndex = 5;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.btnOpciones_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Khaki;
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button8.Location = new System.Drawing.Point(732, 6);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(51, 48);
+            this.button8.TabIndex = 5;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.btnOpciones_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Khaki;
+            this.button9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button9.BackgroundImage")));
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button9.Location = new System.Drawing.Point(737, 3);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(51, 48);
+            this.button9.TabIndex = 5;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.btnOpciones_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(738, 350);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Lab4_2/Lab4_2/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4_2
+{
+    public partial class Form1 : Form
+    {
+        private List<Alimento> alimentos;
+        private List<Bebida> bebidas;
+        private List<Venta> ventas;
+        public Form1()
+        {
+            InitializeComponent();
+            alimentos = new List<Alimento>();
+            bebidas = new List<Bebida>();
+            ventas = new List<Venta>();
+            CargarDatos();
+            MostrarOpciones();
+        }
+
+        private void CargarDatos()
+        {
+            // Cargar datos de alimentos desde el archivo CSV "alimentos.csv"
+            // Cargar datos de bebidas desde el archivo CSV "bebidas.csv"
+            // Cargar datos de precios desde el archivo CSV "precio.csv"
+        }
+
+        private void MostrarOpciones()
+        {
+            tabControl1.SelectedTab = tabPage1;
+        }
+
+        private void MostrarComida()
+        {
+            tabControl1.SelectedTab = tabPage2;
+            //dataGridViewComida.DataSource = alimentos;
+        }
+
+        private void MostrarBebida()
+        {
+            tabControl1.SelectedTab = tabPage3;
+            //dataGridViewBebida.DataSource = bebidas;
+        }
+
+        private void MostrarAjustes()
+        {
+            tabControl1.SelectedTab = tabPage4;
+            //dataGridViewPrecios.DataSource = alimentos;
+            // Configurar la apariencia del DataGridViewPrecios
+        }
+
+        private void MostrarHistorial()
+        {
+            tabControl1.SelectedTab = tabPage5;
+            //dataGridViewHistorial.DataSource = ventas;
+            // Configurar la apariencia del DataGridViewHistorial
+        }
+
+        private void btnComida_Click(object sender, EventArgs e)
+        {
+            MostrarComida();
+        }
+        private void btnOpciones_Click(object sender, EventArgs e)
+        {
+            MostrarOpciones();
+        }
+
+        private void btnBebida_Click(object sender, EventArgs e)
+        {
+            MostrarBebida();
+        }
+
+        private void btnAjustes_Click(object sender, EventArgs e)
+        {
+            MostrarAjustes();
+        }
+
+        private void btnHistorial_Click(object sender, EventArgs e)
+        {
+            MostrarHistorial();
+        }
+
+        private void btnGuardarPrecios_Click(object sender, EventArgs e)
+        {
+            // Guardar los precios en el archivo CSV "precio.csv"
+        }
+
+        private void btnGuardarHistorial_Click(object sender, EventArgs e)
+        {
+            // Guardar el historial de ventas en el archivo CSV "Ventas-[FechaDelDia].csv"
+        }
+
+        private void btnLeerHistorial_Click(object sender, EventArgs e)
+        {
+            // Abrir un cuadro de diálogo para seleccionar un archivo CSV de ventas
+            // Leer los datos del archivo y mostrarlos en el DataGridViewHistorial
+        }
+    }
+
+    // Clase Alimento - Representa un alimento del menú
+
+    public class Alimento
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Imagen { get; set; }
+    }
+
+    // Clase Bebida - Representa una bebida del menú
+
+    public class Bebida
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Imagen { get; set; }
+    }
+
+    // Clase Venta - Representa una venta
+
+    public class Venta
+    {
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        // Agregar propiedades adicionales según el historial de ventas
+    }
+}
